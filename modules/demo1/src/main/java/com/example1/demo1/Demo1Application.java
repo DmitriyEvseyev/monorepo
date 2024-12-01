faf1f31fdf2e87dfaa875749e222abaa0377842a
Dmitriy <yevseyevdmitriy@yandex.ru>
2024-12-02 00:14:09 +0400

    devC

--- modules/demo1/src/main/java/com/example1/demo1/Demo1Application.java
+++ modules/demo1/src/main/java/com/example1/demo1/Demo1Application.java
@@ -8,6 +8,7 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         System.out.println("1111111111");
+        System.out.println("555555");
         SpringApplication.run(Demo1Application.class, args);
     }
 
--- modules/demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ modules/demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -8,6 +8,7 @@ public class Demo2Application {
 
     public static void main(String[] args) {
         System.out.println("22222222");
+        System.out.println("333333");
         SpringApplication.run(Demo2Application.class, args);
     }
 
